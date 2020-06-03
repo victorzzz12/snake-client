67df6f5791f1c0f9aa3b25ccf39496655dd2e340
Victor Liang <victor.zw.liang@gmail.com>
2020-06-03 19:12:09 +0000

    moved input functions to its own file

--- play.js
+++ play.js
@@ -1,24 +1,10 @@
 const {connect} = require('./client');
+const setupInput = require('./input')
 
 console.log('Connecting ...');
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', (handleUserInput));
-  return stdin;
-}
-
-function handleUserInput(key) {
-  if (key === "\u0003") {
-  process.exit();
-}
-};
-
-setupInput();
 connect();
+setupInput();
 
 
 
